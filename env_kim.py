8acad34ce3156047a40d29a6ee560e54e2474726
Yang <hyan212@aucklanduni.ac.nz>
2024-03-01 14:40:33 +1300

    functional rl kim

--- env_kim.py
+++ env_kim.py
@@ -20,11 +20,20 @@ class RL_Kim_TradeEnv(gym.Env):
         obs = np.array([self.df.iloc[self.trade_step*self.tw]['zscore']])
         return obs
     
-    def _get_rwd(self):
-        self.reward = 0
-        return self.reward
+    def _get_rwd(self, W, pos_status):
+        if pos_status == "close":
+            reward = W * 1000
+        elif pos_status == "stoploss":
+            reward = W * -1000
+        elif pos_status == "exit":
+            reward = W * -500
+        else:
+            reward = 0
+        
+        # print(reward)
+        return reward
 
-    def _take_action(self, action):
+    def _take_action(self, cash, action):
         if action == 0:
             trading_boundary = 0.5
             stoploss_boundary = 2.5
@@ -47,11 +56,14 @@ class RL_Kim_TradeEnv(gym.Env):
         trading_df = self.df.iloc[self.trade_step*self.tw:self.trade_step*self.tw+self.tw].reset_index()
 
         holding = [0, 0]
-        cash = 0
         position = 0
+        pos_status = "unopen" # "open", "close", "stoploss", "exit"
         for i, row in trading_df.iterrows():
-            if position==0 and abs(row['zscore']) > trading_boundary:
-                # print("opened pos")
+            # print(i)
+            if position==0 and abs(row['zscore']) > trading_boundary and i<len(trading_df)-1:
+                # print("open")
+                sa, sb = row['close0'], row['close1']
+                pos_status = "open"
                 if row['zscore'] > 0:
                     # Long leg0 short leg1
                     holding = [1/row['close0']*(1-self.tc), -1/row['close1']*(1-self.tc)]
@@ -60,24 +72,32 @@ class RL_Kim_TradeEnv(gym.Env):
                     # Short leg0 long leg1
                     holding = [-1/row['close0']*(1-self.tc), 1/row['close1']*(1-self.tc)]
                     position = 1
-                position = 1
+                # print(holding, sa, sb)
             elif position!=0 and abs(row['zscore']) > stoploss_boundary:
-                # print("stop pos")
-                print(holding)
-                cash = holding[0]*row['close0']*(1-self.tc) + holding[1]*row['close1']*(1-self.tc)
+                # print("stoploss")
+                sat, sbt = row['close0'], row['close1']
+                cash -= holding[0]*row['close0']*(1-self.tc) + holding[1]*row['close1']*(1-self.tc)
                 position = 0
+                pos_status = "stoploss"
                 break
             elif position!=0 and row['zscore']*trading_df.iloc[i-1]["zscore"] < 0:
-                # print("closed pos")
-                print(holding)
-                cash = holding[0]*row['close0']*(1-self.tc) + holding[1]*row['close1']*(1-self.tc)
+                # print("close")
+                sat, sbt = row['close0'], row['close1']
+                cash -= holding[0]*row['close0']*(1-self.tc) + holding[1]*row['close1']*(1-self.tc)
                 position = 0
+                pos_status = "close"
                 break
-            if position==0 and i==len(trading_df)-1:
-                # print("exit pos")   
-                cash = holding[0]*row['close0'] + holding[1]*row['close1']
-        
-        return cash
+            elif i==len(trading_df)-1:
+                # print("exit")   
+                sat, sbt = row['close0'], row['close1']
+                if pos_status == "open":
+                    cash -= holding[0]*row['close0'] + holding[1]*row['close1']
+                    pos_status = "exit"
+
+        # print("end trading window")        
+        # print(pos_status)
+        W = abs(holding[0]*(sat-sa)/sa + holding[1]*(sat-sb)/sb) if pos_status!="unopen" else 0
+        return cash, W, pos_status
 
     def reset(self, seed=None):
         self.trade_step = 0
@@ -89,17 +109,13 @@ class RL_Kim_TradeEnv(gym.Env):
         self.action = action
         self.signal = self.observation
         self.observation = self._get_obs()
-        self.networth = self._take_action(action)
+        self.networth, W, pos_status = self._take_action(self.networth, action)
         self.trade_step += 1
         terminated = self.trade_step >= int(len(self.df)/self.tw)
         truncated = False
 
-        return self.observation, self._get_rwd(), terminated, truncated, {}
+        return self.observation, self._get_rwd(W, pos_status), terminated, truncated, {}
 
     def render(self):
         # print(f"signal: {self.signal}, action: {self.action}, reward:{round(self.reward, 3)}, networth: {round(self.networth, 4)}")
         print(f"networth: {round(self.networth, 4)}")
-
-    def close(self):
-        print("Finished")
-        print(f"networth: {self.networth}")
